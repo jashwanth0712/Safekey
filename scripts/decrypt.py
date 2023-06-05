e78e89ed7820d3faad102cfa3c3d9a1139a8c2f5
jashwanth0712 <jashwanth0712@gmail.com>
2023-06-05 21:52:45 +0530

    Added hide and unhide feature

--- scripts/decrypt.py
+++ scripts/decrypt.py
@@ -1,7 +1,16 @@
 #!/use/bin/env python3
 
 import os
+import subprocess
+import stat
 from cryptography.fernet import Fernet
+#-------------------------functions------------------------------
+def set_write_permission(file_path):
+    # Set write permission for the file
+    try:
+        os.chmod(file_path, stat.S_IWRITE)
+    except:
+        pass
 #function to get all the files in a given folder
 def list_files(directory):
     file_list = []
@@ -10,21 +19,37 @@ def list_files(directory):
             file_list.append(os.path.join(root, file))
     return file_list
 
+def unhide_files(directory):
+    # Unhide individual files within the directory
+    for root, dirs, files in os.walk(directory):
+        for file in files:
+            file_path = os.path.join(root, file)
+            unhide_single_file(file_path)
+
+def unhide_single_file(file_path):
+    # Remove the hidden attribute from the file
+    try:
+        # Execute the command to unhide the file using the command prompt
+        subprocess.run(['attrib', '-h', file_path], shell=True)
+    except:
+        pass
+
+
+#------------------------------------------------------
 path=r'C:\Users\jashw\OneDrive\Desktop\Pangea\Safekey\scripts\test'
-files=list_files(path)
+unhide_files(path)
+files = list_files(path)
 print(files)
-#generating a key
-key=''
-with open("thekey.key","rb") as key:
-    key=key.read()
+# Generating a key
+key = ''
+with open("thekey.key", "rb") as key_file:
+    key = key_file.read()
 print(key)
 
-with open("thekey.key","wb")as thekey:
-    thekey.write(key)
-
 for file in files:
-    with open(file,"rb") as thefile:
-        contents=thefile.read()
+    with open(file, "rb") as thefile:
+        contents = thefile.read()
     contents_decrypted = Fernet(key).decrypt(contents)
-    with open(file,"wb") as thefile:
+    set_write_permission(file)  # Set write permission
+    with open(file, "wb") as thefile:
         thefile.write(contents_decrypted)
