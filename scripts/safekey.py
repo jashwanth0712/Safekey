f2c12174ed818a5d82cbe39bd54e83f365bab5f1
jashwanth0712 <jashwanth0712@gmail.com>
2023-06-10 12:08:53 +0530

    updated saefkey.py script

--- scripts/safekey.py
+++ scripts/safekey.py
@@ -1,17 +1,77 @@
 import argparse
+import subprocess
+import os
+
+is_get = False
+file_paths = []
+folder_path = ""
+is_usb = False
+def read_file(file):
+    if not os.path.isfile(file):
+        return None
+
+    with open(file, "rb") as thefile:
+        contents = thefile.read().decode("utf-8")
+    return contents
+
+
+def get_current_pendrives():
+    out = subprocess.check_output('wmic logicaldisk get DriveType, Caption', shell=True)
+    current_disks = []
+    for drive in str(out).strip().split('\\r\\r\\n'):
+        if '2' in drive:
+            drive_info = drive.split(':')
+            drive_letter = drive_info[0]
+            drive_type = drive_info[1].strip()
+            if drive_type == '2':
+                current_disks.append(drive_letter)
+                print(drive_letter," ",read_file(f'{drive_letter}:\\thekey.key'))
+    
+def get_function():
+    print("Get")
+
+def set_function():
+    print("Set")
+
+def list_function():
+    print("List of file paths:", file_paths)
+
+def folder_function():
+    print("Folder path:", folder_path)
+
 
 def parse_arguments():
+    global is_get, file_paths, folder_path, is_usb
+
     parser = argparse.ArgumentParser(description='CLI Tool')
-    parser.add_argument('--name', help='Specify your name', required=True)
-    parser.add_argument('--age', help='Specify your age', type=int, required=True)
-    return parser.parse_args()
+    group = parser.add_mutually_exclusive_group(required=True)
+    group.add_argument('-g', '--get', action='store_true', help='Get')
+    group.add_argument('-gu', '--getusb', action='store_true', help='USB')
+    group.add_argument('-s', '--set', action='store_true', help='Set')
+    group.add_argument('-l', '--list', nargs='+', help='List of file paths')
+    group.add_argument('-f', '--folder', help='Folder path')
 
-def handle_arguments(args):
-    print(f'Hello, {args.name}! You are {args.age} years old.')
+    args = parser.parse_args()
 
-def main():
-    args = parse_arguments()
-    handle_arguments(args)
+    if args.get:
+        is_get = True
+        get_function()
+    elif args.set:
+        is_get = False
+        set_function()
+    elif args.list:
+        file_paths = args.list
+        list_function()
+    elif args.folder:
+        folder_path = args.folder
+        folder_function()
+    elif args.getusb:
+        is_usb = True
 
+
+    #set files 
+    if(is_usb):
+        get_current_pendrives()
+            
 if __name__ == '__main__':
-    main()
+    parse_arguments()
